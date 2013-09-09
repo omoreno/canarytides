0e09b57faa7edcd382ad12b96aaf8b8085b67632
omoreno <oscar.moreno.martin@gmail.com>
2013-09-09 23:33:09 +0100

    refactor, extract class

--- src/CanaryTides.js
+++ src/CanaryTides.js
@@ -3,36 +3,41 @@ window.CanaryTides = window.CanaryTides || {};
 (function(CanaryTides, undefined) {
 	
 	/******* Widgets *******/
-	function TextBoxWidget(elementId){
-		var self = this;
-		this.elementId = elementId;
+	function Widget(elementId){
+		this.elementId= elementId;
 
-		var createDomElement = function () {
-			var element = $('<input>', { type: "text", id: self.elementId });
+		var attachToDOM = function(element){
 			$("body").append(element);
 		};
-
+		
 		this.initialize = function(){
-			createDomElement();
+			var element = this.createElement();
+			attachToDOM(element);
 		};
 
+		this.createElement = function(){};//to be overrided
 	};
+
+	function TextBoxWidget(elementId){
+		Widget.call(this, elementId);
+		
+		this.createElement = function () {
+			return $('<input>', { type: "text", id: this.elementId });
+		};
+	};
+	TextBoxWidget.prototype = new Widget();
+	TextBoxWidget.prototype.constructor = TextBoxWidget;
 
 	function ButtonWidget(elementId){
-		var self = this;
-		this.elementId = elementId;
-
-		var createDomElement = function () {
-			var element = $('<button>', { id: self.elementId });
-			$("body").append(element);
+		Widget.call(this, elementId);
+		
+		this.createElement = function () {
+			return $('<button>', { id: this.elementId });
 		};
-
-		this.initialize = function(){
-			createDomElement();
-		};
-
 	};
-	
+	ButtonWidget.prototype = new Widget();
+	ButtonWidget.prototype.constructor = ButtonWidget;
+
 	CanaryTides.Widgets = CanaryTides.Widgets || {};
 	CanaryTides.Widgets.TextBox = TextBoxWidget;
 	CanaryTides.Widgets.Button = ButtonWidget;
